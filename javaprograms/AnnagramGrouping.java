85a55ba187b7b835485fb6a56c96ea6e2ce45153
Darshua <dk.darshan1992@gmail.com>
2024-03-24 22:21:38 +0530

    AnnagramGroupingProgram

---
+++ javaprograms/AnnagramGrouping.java
@@ -0,0 +1,19 @@
+package interviewPrograms;
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class AnnagramGrouping {
+    public static void main(String[] args) {
+        String[] items = {"eat", "ate", "tea", "nat", "tan"};
+        new HashSet<>((Stream.of(items).collect(Collectors.toMap(item -> item, item -> Stream.of(item.split("")).sorted().collect(Collectors.joining())))).values()).stream().collect(Collectors.toMap(key -> key, val ->
+                (Stream.of(items).collect(Collectors.toMap(item -> item, item -> Stream.of(item.split("")).sorted().collect(Collectors.joining())))).entrySet().stream().filter(entry -> entry.getValue().equalsIgnoreCase(val)).map(Map.Entry::getKey).collect(Collectors.toList()))).forEach((b, n) -> System.out.println(b + "  " + n));
+
+    }
+}
+
+
