6644749111e38c5f7859b544367207dbf3b96c0f
Darshua <dk.darshan1992@gmail.com>
2024-03-25 00:36:33 +0530

    AnnagramGroupingProgram

--- Anagram.java
+++ Anagram.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-class AnnagramGrouping {
+class Anagram {
     public static void main(String[] args) {
         String[] items = {"eat", "ate", "tea", "nat", "tan"};
         new HashSet<>(
